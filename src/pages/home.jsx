e8bed0c6b3e73bbf72199c6785a9cbc8462e5649
Dev Shakya <devxoshakya@gmail.com>
2024-08-03 21:33:16 +0530

    added watch history

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Slideshow from '../components/home/slideshow';
 import { fetchTrendingAnime, fetchPopularAnime} from '../hooks/useAPI';
 import CardGrid from '../components/cards/cardItem';
+import EpisodeCard from '../components/home/EpisodeCard';
 
 const Home = () => {
   window.scrollTo(0, 0);
@@ -12,6 +13,8 @@ const Home = () => {
 
   const [trendingData, setTrendingData] = useState([]);
   const [popularData, setPopularData] = useState([]);
+  const [state, setState] = useState({ watchedEpisodes: [] });
+
 
   useEffect(() => {
     fetchTrendingAnime()
@@ -39,6 +42,26 @@ const Home = () => {
       });
   }, []);
 
+  useEffect(() => {
+    const fetchWatchedEpisodes = () => {
+        const watchedEpisodesData = localStorage.getItem('watched-episodes');
+        if (watchedEpisodesData) {
+            const allEpisodes = JSON.parse(watchedEpisodesData);
+            const latestEpisodes = [];
+            Object.keys(allEpisodes).forEach((animeId) => {
+                const episodes = allEpisodes[animeId];
+                const latestEpisode = episodes[episodes.length - 1];
+                latestEpisodes.push(latestEpisode);
+            });
+            setState((prevState) => ({
+                ...prevState,
+                watchedEpisodes: latestEpisodes,
+            }));
+        }
+    };
+
+    fetchWatchedEpisodes();
+}, []);
   
 
   useEffect(() => {
@@ -90,6 +113,7 @@ const Home = () => {
   return (
     <div className='text-white my-16'>
       <Slideshow data={trendingData} />
+      <EpisodeCard />
       <div className='flex md:flex-col'>
       <div className=" w-full mx-auto md:w-full p-4">
         <div className="flex mx-auto justify-center gap-4 my-8">
